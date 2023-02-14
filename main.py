698fd836d2de0e7a213bcff6b344ed7a86295405
Roger Monterroso <rogerclad228@gmail.com>
2023-02-14 09:52:37 -0600

    Actualizacion Gitignore y Python
    Se creo la carpeta modulos, ignoré la carpeta modulos

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 print("Nombre: Roger Leonardo Monterroso Recinos")
 print("Carné: 1565723")
+
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
